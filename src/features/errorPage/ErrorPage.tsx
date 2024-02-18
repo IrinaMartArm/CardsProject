8c1de0edb637656b9005a7118213fb87f8b84f60
Ирина Мартоян <surenira@icloud.com>
2024-02-19 01:12:11 +0400

    Title block

--- src/features/errorPage/ErrorPage.tsx
+++ src/features/errorPage/ErrorPage.tsx
@@ -8,7 +8,7 @@ import error from '../../components/assets/images/404.png'
 export const ErrorPage = () => {
   return (
     <div className={s.root}>
-      <Header withBtn />
+      <Header name={'Bob'} withBtn />
       <div className={s.content}>
         <img alt={'errorPage ERROR'} className={s.img} src={error} />
         <Typography variant={'body2'}>Sorry! Page not found!</Typography>
