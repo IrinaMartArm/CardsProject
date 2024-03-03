4a2591830904810f47a2b5a62cd0b661a2c94f74
IlyaZolotukhin <1zolotyhin1@gmail.com>
2024-03-03 17:29:15 +0700

    fix style

--- src/components/ui/modals/dialogs/DeleteCardDialog.tsx
+++ src/components/ui/modals/dialogs/DeleteCardDialog.tsx
@@ -1,7 +1,6 @@
 import { TrashBin } from '@/components/assets/icons'
 import { Button, Modal, Typography } from '@/components/ui'
 import { ModalClose } from '@/components/ui/modals/ModalClose'
-import { clsx } from 'clsx'
 
 import s from '../Modals.module.scss'
 
@@ -18,7 +17,7 @@ export const DeleteCardDialog = ({ className, disabled, name, onClick }: Props) 
     <Modal
       title={'Delete Card'}
       trigger={
-        <div className={clsx(className)}>
+        <div className={className}>
           <TrashBin />
           {name}
         </div>
